01a5b524f846009ebe02089e3f32e5e2bb4acda9
Oskar Schöldström <m@oxy.fi>
2018-05-31 15:05:35 -0300

    timber integration fixes

--- src/Common/Templating.php
+++ src/Common/Templating.php
@@ -9,21 +9,26 @@ trait Templating
     public function template($slug, $attributes)
     {
         $handle = Plugin::get_instance()->plugin_name;
-        $templates = apply_filters("$handle/template_hierarchy", [
-            "$slug.twig",
+        $templates = [
             "$slug.php",
-        ]);
+        ];
+
+        $templates = apply_filters("$handle/template_hierarchy", array_reverse(array_merge($templates, array_map(function ($template) {
+            return 'gutenberg/' . $template;
+        }, $templates))));
+
+        // WP Timber Extended support
+        $templates = apply_filters('timber_template_hierarchy', $templates);
 
         $template = locate_template($templates);
         if ($template = apply_filters("$handle/template", $template)) {
             // If it's not twig, render as php
             if (substr($template, -5) !== '.twig') {
                 return $this->renderPhpTemplate($template, $attributes);
-            } else {
-                // If the path is absolute, use the relative path from the theme.
-                $template = str_replace(TEMPLATEPATH, '', $template);
-                return Timber::fetch($template, $attributes);
             }
+            // If the path is absolute, use the relative path from the theme.
+            $template = str_replace(TEMPLATEPATH, '', $template);
+            return \Timber::fetch($template, $attributes);
         }
         return $this->renderPhpTemplate($this->getDir() . "/views/$slug.php", $attributes);
     }
@@ -33,7 +38,7 @@ trait Templating
         return dirname((new \ReflectionClass(static::class))->getFileName());
     }
 
-    public function renderPhpTemplate($path, $attributes = [])
+    public function renderPhpTemplate($path, $attributes)
     {
         extract($attributes);
         ob_start();

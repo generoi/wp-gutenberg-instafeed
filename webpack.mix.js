34d8b27838024e05fe2b9ab6801b0ec47d84bdf5
ben <lonnqvistben@gmail.com>
2018-08-16 08:44:44 +0300

    Feed can now scroll. can't reproduce df1c54064c617aea89b8fe712094ebbec2c8091b

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,8 +7,8 @@ mix.react('src/index.js', 'dist/');
 mix.babelConfig({
   'plugins': [
     [
-      '@wordpress/babel-plugin-makepot', { 'output': 'languages/javascript.pot' }
-    ]
+      '@wordpress/babel-plugin-makepot', { 'output': 'languages/javascript.pot' },
+    ],
   ],
 });
 
@@ -31,4 +31,3 @@ mix.webpackConfig({
     '@wordpress/apiRequest': 'wp.apiRequest',
   },
 });
-

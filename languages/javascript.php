f71d043993cb155cf86ecf2fa8c0cb80f80a8d50
Oskar Schöldström <m@oxy.fi>
2019-06-04 12:42:53 -0300

    translations

--- languages/javascript.php
+++ languages/javascript.php
@@ -1,49 +1,49 @@
 <?php
 /* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
 $generated_i18n_strings = array(
-	// Reference: src/edit.js:101
+	// Reference: src/edit.js:102
 	__( 'Add a gutter between images.', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:107
+	// Reference: src/edit.js:108
 	__( 'Wrap the images with a link to the photo on Instagram.', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:113
+	// Reference: src/edit.js:114
 	__( 'Display caption.', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:136
+	// Reference: src/edit.js:137
 	__( '@username or #hashtag', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:156
+	// Reference: src/edit.js:157
 	__( 'Type a username or hashtag to search for in the field above...', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:61
+	// Reference: src/edit.js:62
 	__( 'thumbnail (150x150)', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:62
+	// Reference: src/edit.js:63
 	__( 'small (240x240)', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:63
+	// Reference: src/edit.js:64
 	__( 'medium (320x320)', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:64
+	// Reference: src/edit.js:65
 	__( 'large (640x640)', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:68
+	// Reference: src/edit.js:69
 	__( 'masonry', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:69
+	// Reference: src/edit.js:70
 	__( 'grid', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:70
+	// Reference: src/edit.js:71
 	__( 'fit together', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:77
+	// Reference: src/edit.js:78
 	__( 'Layout', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:85
+	// Reference: src/edit.js:86
 	__( 'Limit', 'wp-gutenberg-instafeed' ),
 
-	// Reference: src/edit.js:93
+	// Reference: src/edit.js:94
 	__( 'Resolution', 'wp-gutenberg-instafeed' ),
 
 	// Reference: src/index.js:16
